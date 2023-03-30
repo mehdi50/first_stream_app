d5d54fb3336eb9ddd250adca4f9f5d5d2fc9f69f
mehdi50 <48949215+mehdi50@users.noreply.github.com>
2023-03-30 16:21:25 +0200

    Update streamlit_app.py
    ajout menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@ import streamlit
 
 streamlit.title('My parents new healthy Dinner')
 
+streamlit.header('Breakfeast Menu')
+
+streamlit.text('Omega 3 and blueberry Oatmeal ')
+
+streamlit.text('Kale, spinach and Rocket smoothies')
+
+streamlit.text('HArd-boiled Free-Range Egg')
